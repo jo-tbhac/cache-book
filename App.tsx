0e7763ef665cd1b7cdd4c59f303c3602bde1f1a8
junpei ogawa <cornerstone.am3@gmail.com>
2022-06-25 20:58:11 +0900

    set up eslint

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
