cf2fdad109befa4646c11e3129ec43118977a913
junpei ogawa <cornerstone.am3@gmail.com>
2022-06-26 18:13:36 +0900

    set up import absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '@components': './src/components',
+            '@navigators': './src/navigators',
+          },
+        },
+      ],
+    ],
   };
 };
